59b5c031723651e85659df37bccf678a9ab83cc9
EmmanuelBett <85231611+Emmanuelbett67@users.noreply.github.com>
2024-01-31 07:31:03 +0300

    Add files via upload

---
+++ Ask-her-out-main/Ask-her-out-main/script.js
@@ -0,0 +1,38 @@
+function startQuestions() {
+    document.getElementById("questions").style.display = "block";
+  }
+  
+  function answerYes() {
+    document.getElementById("questions").style.display = "none";
+      document.getElementById("que").style.display = "none";
+      document.getElementById("init").innerText = "YEY YEY 🎉🎉🎉";
+    document.getElementById("message").style.display = "block";
+    document.getElementById("messageText").innerText = "Yayyy~ Bett is happy, see you then!!";
+    showConfetti()
+}
+  
+  function answerNo() {
+    document.getElementById("questions").style.display = "none";
+    document.getElementById("que").style.display = "none";
+    document.getElementById("init").innerText = "YEY YEY 🎉🎉🎉";
+    document.getElementById("message").style.display = "block";
+    document.getElementById("messageText").innerText = "You mean no, like capital N, O? Fine Bett is sad :(";
+    showConfetti()
+
+}
+  
+// Function to show confetti
+function showConfetti() {
+    const confettiSettings = {
+      target: 'confetti-canvas',
+      respawn: true,
+      colors: ['#ff0000', '#00ff00', '#0000ff'], 
+    };
+  
+    const confetti = new ConfettiGenerator(confettiSettings);
+    confetti.render();
+    setTimeout(() => {
+      confetti.clear();
+    }, 5000);
+  }
+  
